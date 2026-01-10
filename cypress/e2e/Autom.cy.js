b2fcb141d246df761784eee2f1324eb50aff3d0a
Isaac Neto <pogorelskyisaac@gmail.com>
2026-01-10 19:53:48 -0300

    isaac teste

--- cypress/e2e/Autom.cy.js
+++ cypress/e2e/Autom.cy.js
@@ -1,5 +1,5 @@
 describe('Cypress e Github Action', () => {
-  it('Visitando o site e testando', () => {
+  it('Visitando o site e testandhgo', () => {
     cy.visit('../src/index.html')
     cy.title().should('eq' , 'Curso de Cypress + Github Actions')
   })
